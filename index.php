d49006f112a6115791e80ba9c481bde6a733c854
Can <yamahasirossi@gmail.com>
2022-06-07 22:22:00 +0300

    unnecessary commit

--- index.php
+++ index.php
@@ -4,10 +4,5 @@ header('Content-type: application/json');
 require __DIR__. '/vendor/autoload.php';
 
 $f = new FrontController();
-$cont = new HelloWorldController();
-$f->registerRoute("GET" ,"/HelloWorld", new HelloWorldndexCommand($cont));
-$f->registerRoute("GET" ,"/SetNum/([0-9]+)/([0-9]+)", new HelloWorldSetNumCommand($cont));
-$f->registerRoute("POST", "/register", new HelloWorldRegisterCommand($cont));
-$f->registerRoute("POST", "/login", new HelloWorldLoginCommand($cont));
 
 $f->run($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
