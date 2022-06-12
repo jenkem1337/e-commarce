004920248f27c2487216069cd9b20cde61d9876e
Can <yamahasirossi@gmail.com>
2022-06-12 14:36:01 +0300

    added jwt and refresh token method

--- src/controller/AuthController.php
+++ src/controller/AuthController.php
@@ -3,6 +3,11 @@
 
 require './vendor/autoload.php';
 use Ramsey\Uuid\Nonstandard\Uuid;
+use Firebase\JWT\JWT;
+
+$dotenv = Dotenv\Dotenv::createImmutable("C:\\xampp\htdocs\\");
+$dotenv->load();
+
 
 class AuthController {
     private AuthService $authService;
@@ -13,9 +18,31 @@ class AuthController {
     }
 
     function login(){
+
         $jsonBody = json_decode(file_get_contents('php://input'));
         $userLoginDto = new UserLoginDto($jsonBody->email, $jsonBody->password); 
         $response = $this->authService->login($userLoginDto);
+        if($response->isSuccess()){
+           $refreshTokenModel = $response->getRefreshToken();
+           $issuedAt = time();
+           $expireTime = $issuedAt + (60 * 60 * 24);
+            $payload = [
+                "exp"=> $expireTime ,
+                "iat"=>$issuedAt,
+                "user_uuid"=>$response->getUuid(),
+                "full_name"=>$response->getFullName(),
+                "email"=>$response->getEmail(),
+                "user_role"=>$response->getUserRole()
+            ];
+            
+            $token = JWT::encode($payload,  $_ENV["SECRET_KEY"], "HS256");
+            echo json_encode([
+                "message"=>"Login Process Succesful",
+                "access_token"=> $token,
+                "refresh_token"=>$refreshTokenModel->getRefreshToken()
+            ]);
+            die();
+        }
     }
 
     function register(){
@@ -45,6 +72,36 @@ class AuthController {
             die();
         }
 
+    }
+
+    function refreshToken(){
+            
+            $jsonBody = json_decode(file_get_contents('php://input'));
+        
+            $response = $this->authService->refreshToken(new RefreshTokenDto($jsonBody->refresh_token));
+            if($response->isSuccess()){
+                $refreshTokenModel = $response->getRefreshToken();
+                $issuedAt = time();
+                $expireTime = $issuedAt + (60 * 60 * 24);
+                 $payload = [
+                     "exp"=> $expireTime ,
+                     "iat"=>$issuedAt,
+                     "user_uuid"=>$response->getUuid(),
+                     "full_name"=>$response->getFullName(),
+                     "email"=>$response->getEmail(),
+                     "user_role"=>$response->getUserRole()
+                 ];
+                 
+                 $token = JWT::encode($payload,  $_ENV["SECRET_KEY"], "HS256");
+                 echo json_encode([
+                     "message"=>"Token Refreshed",
+                     "access_token"=> $token,
+                     "refresh_token"=>$refreshTokenModel->getRefreshToken()
+                 ]);
+                 die();
+             }
+    
+ 
     }
     function verifyUserAccount(){
         $code = isset($_GET['code']) ? $_GET['code'] :"";
