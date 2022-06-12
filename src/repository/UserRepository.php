8976b9bfe5cb54619309a445b062a5ce62085af6
Can <yamahasirossi@gmail.com>
2022-06-12 14:37:48 +0300

    lots of method added

--- src/repository/UserRepository.php
+++ src/repository/UserRepository.php
@@ -1,8 +1,17 @@
 <?php
 
 interface UserRepository{
-    function findUserByEmail($userId);
+    function findUserByEmail($userId):User;
     function save(User $user);
-    function findUserByVerificationCode($code);
+    function findUserByVerificationCode($code):User;
     function updateUserActivatedState(User $user);
+    function saveRefreshToken(User $user);
+    function findUserByUuidWithRefreshToken($refreshToken);
+    function updatePassword(User $user);
+    function updateForgettenPasswordCode(User $user);
+    function findUserByForgettenPasswordCode($passwordCode);
+    function findAllWithPagination($startingLimit, $perPageForUsers);
+    function findOneUserByUuid($uuid);
+    function updateFullName(User $user);
+
 }
