94451662d4acf2cf5278807bb1aede9a4d3e02b5
Can <yamahasirossi@gmail.com>
2022-06-14 12:56:16 +0300

    UserRepositoryAggregateRootDecorator added

--- src/factory/controller/AuthControllerFactory.php
+++ src/factory/controller/AuthControllerFactory.php
@@ -3,6 +3,12 @@ require "./vendor/autoload.php";
 use PHPMailer\PHPMailer\PHPMailer;
 class AuthControllerFactory implements Factory{
     function createInstance(...$params):AuthController {
-        return new AuthController(new AuthServiceImpl(new UserRepositoryImpl(new UserDaoImpl(new MySqlPDOConnection()), new RefreshTokenDaoImpl(new RedisConnection())), new EmailServiceImpl(new PHPMailer(true))));
+        return new AuthController(
+            new AuthServiceImpl(
+                new UserRepositoryAggregateRootDecorator(
+                    new UserRepositoryImpl(
+                        new UserDaoImpl(new MySqlPDOConnection()), 
+                        new RefreshTokenDaoImpl(new RedisConnection()))), 
+                new EmailServiceImpl(new PHPMailer(true))));
     }
 }
--- src/factory/controller/UserControllerFactory.php
+++ src/factory/controller/UserControllerFactory.php
@@ -1,12 +1,27 @@
 <?php
 
-use UserController as GlobalUserController;
-
 require "./vendor/autoload.php";
 use PHPMailer\PHPMailer\PHPMailer;
 class UserControllerFactory implements Factory {
     
     function createInstance(...$params):UserController {
-        return new UserController(new UserServiceImpl(new UserRepositoryImpl(new UserDaoImpl(new MySqlPDOConnection()),new RefreshTokenDaoImpl(new RedisConnection()))), new AuthServiceImpl(new UserRepositoryImpl(new UserDaoImpl(new MySqlPDOConnection()), new RefreshTokenDaoImpl(new RedisConnection())), new EmailServiceImpl(new PHPMailer(true))));
+        return new UserController(
+            new UserServiceImpl(
+                new UserRepositoryAggregateRootDecorator(
+                    new UserRepositoryImpl(
+                        new UserDaoImpl(new MySqlPDOConnection()),
+                        new RefreshTokenDaoImpl(new RedisConnection())
+                    )
+                )
+            ),new AuthServiceImpl(
+                new UserRepositoryAggregateRootDecorator(
+                    new UserRepositoryImpl(
+                        new UserDaoImpl(new MySqlPDOConnection()),
+                        new RefreshTokenDaoImpl(new RedisConnection())
+
+                    )
+                ),
+                new EmailServiceImpl(new PHPMailer(true)))
+        );
     }
 }
