bef722147ae90964711ac5f6458efde9fc09d53b
Can <yamahasirossi@gmail.com>
2022-06-17 12:54:32 +0300

    explode function removed

--- src/front_controller/ExceptionHandler.php
+++ src/front_controller/ExceptionHandler.php
@@ -2,15 +2,11 @@
 
 class ExceptionHandler {
     function handle(Exception $error){
-        $splitedException = explode(', ', $error->getMessage());
-        $message = $splitedException[0];
-        $statusCode = $splitedException[1];
-        if(!$statusCode) $statusCode = 500;
         echo json_encode([
-            "error-message"=>$message,
-            "status_code"=>$statusCode
+            "error_message"=>$error->getMessage(),
+            "status_code"=>$error->getCode()
         ]);
-        http_response_code($statusCode);
+        http_response_code($error->getCode());
         die();
     }
 }
