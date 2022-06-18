2b722199b49a8ee2bc82030bcc41a7ee4d989bf5
Can <yamahasirossi@gmail.com>
2022-06-18 13:10:00 +0300

    initial commit

---
+++ src/model/user/UserInteface.php
@@ -0,0 +1,51 @@
+<?php
+
+interface UserInterface {
+        function changeFullName($newFullname);
+
+        function hashPassword($userPassword);
+    
+        function createNewPassword($newPassword);
+    
+        function ChangePassword($oldPassword,$newPassword);
+    
+        function comparePassword($fromBody):bool;
+    
+        function isUserActiveted();
+    
+        function createForgettenPasswordCode();
+    
+        function createActivationCode();
+    
+        function setActivationCode($code);
+    
+        function setRefreshTokenModel(RefreshToken $refToken);
+    
+        public function setUserRole($userRole);
+    
+        function getRefreshTokenModel();
+    
+        public function getFullname();
+    
+        public function getEmail();
+    
+        public function getPassword();
+    
+        public function getActivationCode();
+    
+        public function getUserRole();
+    
+        function getForegettenPasswordCode();
+    
+        public function getIsUserActiveted();
+        
+        public function getUuid();
+
+        public function getCreatedAt();
+    
+        public function getUpdatedAt();
+
+        function isNull();
+    
+    
+}
