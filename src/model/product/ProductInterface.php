4525fdccd66d7e632573d2586e45208677e3d12a
Can <yamahasirossi@gmail.com>
2022-06-23 15:11:58 +0300

    Interface name changed and added new methods

--- src/model/product/ProductInterface.php
+++ src/model/product/ProductInterface.php
@@ -1,28 +1,26 @@
 <?php
 
-interface ProductInteface {
+interface ProductInterface {
         function incrementStockQuantity(int $quantity);
         function decrementStockQuantity(int $quantity);
         function changeHeader($header);
     
         function changeDescription($description);
-    
+        function setRate(RateInterface $rate);
         function changePrice($price);
-    
+        function calculateAvarageRate();
         function isPriceLessThanPreviousPrice();
         function addSubscriber(ProductSubscriberInterface $sub);
         function addCategory(CategoryInterface $category);
-    
+        function addRate(RateInterface $rate);
         function addComment(CommentInterface $comment);
         function addImage(ImageInterface $img);
         public function getBrand();
-    
         public function getModel();
     
         public function getPrice();
     
         public function getStockQuantity();
-    
         public function getRate():RateInterface;
     
         public function getComments():CommentCollection;
@@ -30,7 +28,8 @@ interface ProductInteface {
         public function getDescription();
     
         public function getHeader();
-        
+        public function getRates():RateCollection;
+
         public function getImages():ImageCollection;
     
         public function getSubscribers():SubscriberCollection;
@@ -39,6 +38,7 @@ interface ProductInteface {
 
         public function getUuid();
 
+        public function getPreviousPrice();
         public function getCreatedAt();
 
         public function getUpdatedAt();
