63606fe3c3540bb0aae04a58f4d411be2c997a5c
Can <yamahasirossi@gmail.com>
2022-06-23 15:15:26 +0300

    added code

--- src/exception/NegativeValueException.php
+++ src/exception/NegativeValueException.php
@@ -3,6 +3,6 @@ require "./vendor/autoload.php";
 class NegativeValueException extends BaseException {
     function __construct()
     {
-        parent::__construct("value must not be negative value");
+        parent::__construct("value must not be negative value", 400);
     }
 }
