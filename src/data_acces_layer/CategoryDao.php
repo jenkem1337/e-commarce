b67ae2b724afebe874af048e108c5ce7c1202b85
Can <yamahasirossi@gmail.com>
2022-07-18 20:44:10 +0300

    Category object refarances deleted

--- src/data_acces_layer/CategoryDao.php
+++ src/data_acces_layer/CategoryDao.php
@@ -1,9 +1,9 @@
 <?php
 interface CategoryDao {
     function persist(Category $c);
-    function deleteByUuid(Category $c);
-    function findByUuid(Category $c);
-    function updateByUuid(Category $c);
+    function deleteByUuid($uuid);
+    function findByUuid($uuid);
+    function updateNameByUuid($uuid, $categoryName);
     function findAll();
     function addCategoryUuidToProduct($productUuid);
 }
--- src/data_acces_layer/Implemantions/CategoryDaoImpl.php
+++ src/data_acces_layer/Implemantions/CategoryDaoImpl.php
@@ -9,7 +9,7 @@ class CategoryDaoImpl implements CategoryDao {
 	}
 	function persist(Category $c) {
         $conn = $this->databaseConnection->getConnection();
-        $stmt = $conn->prepare("INSERT INTO category (uuid, category_name, created_at, updated_at) VALUES (:uuid, :category_name, :created_at, uypdated_at)");
+        $stmt = $conn->prepare("INSERT INTO category (uuid, category_name, created_at, updated_at) VALUES (:uuid, :category_name, :created_at, :updated_at)");
         $stmt->execute([
             'uuid'=>$c->getUuid(),
             'category_name' => $c->getCategoryName(),
@@ -19,20 +19,20 @@ class CategoryDaoImpl implements CategoryDao {
         $conn = null;
     }
 	
-	function deleteByUuid(Category $c) {
+	function deleteByUuid($uuid) {
         $conn = $this->databaseConnection->getConnection();
         $stmt = $conn->prepare("DELETE FROM category WHERE uuid = :uuid");
         $stmt->execute([
-            'uuid'=> $c->getUuid()
+            'uuid'=> $uuid
         ]); 
        $conn = null; 
 	}
 	
-	function findByUuid(Category $c) {
+	function findByUuid($uuid) {
         $conn = $this->databaseConnection->getConnection();
         $stmt = $conn->prepare("SELECT * FROM category WHERE uuid = :uuid LIMIT 1");
         $stmt->execute([
-            'uuid'=>$c->getUuid()
+            'uuid'=>$uuid
         ]);
         $category = $stmt->fetch(PDO::FETCH_OBJ);
         $conn = null; 
@@ -40,12 +40,12 @@ class CategoryDaoImpl implements CategoryDao {
         return $category;
 	}
 	
-	function updateByUuid(Category $c) {
+	function updateNameByUuid($uuid, $newCategoryName) {
         $conn = $this->databaseConnection->getConnection();
         $stmt = $conn->prepare("UPDATE category SET category_name = :category_name WHERE uuid = :uuid");
         $stmt->execute([
-             'category_name'=>$c->getCategoryName(),
-             'uuid'=> $c->getUuid()
+             'category_name'=>$newCategoryName,
+             'uuid'=> $uuid
         ]);
         $conn = null;
 	}
