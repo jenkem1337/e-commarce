fc2546921a661ba9a0de4453e94812a14973f849
Can <yamahasirossi@gmail.com>
2022-07-20 17:18:24 +0300

    initial commit

--- src/tests/integration/data_acces/image/ImageInMemoryDaoTest.php
+++ src/tests/integration/data_acces/image/ImageInMemoryDaoTest.php
@@ -11,4 +11,35 @@ class ImageInMemoryDaoTest extends TestCase {
     function setUp():void {
         $this->dao = new ImageInMemoryDaoImpl(new SqliteInMemoryConnection());
     }
+    function test_persist()
+    {
+        $this->dao->persist(new Image(uuid::uuid4(), uuid::uuid4(),'example.jpg', date('h'), date('h')));
+        $this->assertEquals(1, count($this->dao->findAll()));
+    }
+    function test_delete_by_uuid(){
+        $imageUuid = uuid::uuid4();
+        $this->dao->persist(new Image($imageUuid, uuid::uuid4(),'example.jpg', date('h'), date('h')));
+        $this->dao->persist(new Image(uuid::uuid4(), uuid::uuid4(),'sadasd.jpg', date('h'), date('h')));
+        $this->dao->persist(new Image(uuid::uuid4(), uuid::uuid4(),'exaadasdadmple.jpg', date('h'), date('h')));
+        $this->dao->persist(new Image(uuid::uuid4(), uuid::uuid4(),'exaaaaacvxcvxcvxcvmple.jpg', date('h'), date('h')));
+        $this->dao->deleteByUuid($imageUuid);
+        $this->assertEquals(3, count($this->dao->findAll()));
+    }
+    function test_find_one_by_uuid()
+    {
+        $imageUuid = uuid::uuid4();
+        $this->dao->persist(new Image($imageUuid, uuid::uuid4(),'example.jpg', date('h'), date('h')));
+        $this->dao->persist(new Image(uuid::uuid4(), uuid::uuid4(),'sadasd.jpg', date('h'), date('h')));
+        $imgFromDao = $this->dao->findOneByUuid($imageUuid);
+        $this->assertEquals('example.jpg', $imgFromDao->image_name);
+    }
+    function test_find_one_by_product_uuid()
+    {
+        $productUuid = uuid::uuid4();
+        $this->dao->persist(new Image(uuid::uuid4(), $productUuid,'sadasd.jpg', date('h'), date('h')));
+        $this->dao->persist(new Image(uuid::uuid4(), uuid::uuid4(),'gdfgdfgsdfg.jpg', date('h'), date('h')));
+        $this->dao->persist(new Image(uuid::uuid4(), uuid::uuid4(),'sareterweedasd.jpg', date('h'), date('h')));
+        $imgFromDao = $this->dao->findImageByProductUuid($productUuid);
+        $this->assertEquals('sadasd.jpg', $imgFromDao->image_name);
+    }
 }
