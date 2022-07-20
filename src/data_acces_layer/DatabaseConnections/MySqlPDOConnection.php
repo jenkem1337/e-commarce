51f05caa01da788ab468dc67557cd69693e450e5
Can <yamahasirossi@gmail.com>
2022-07-20 21:52:48 +0300

    replaced to singleton instance

--- src/data_acces_layer/DatabaseConnections/MySqlPDOConnection.php
+++ src/data_acces_layer/DatabaseConnections/MySqlPDOConnection.php
@@ -1,20 +1,27 @@
 <?php
 require './vendor/autoload.php';
 
-
-class MySqlPDOConnection implements DatabaseConnection{
+class MySqlPDOConnection implements SingletonConnection{
+    private static $instace;
     private $pdo;
     private $databaseHost;
     private $databaseName;
-    function __construct()
+    private function __construct()
     {
-        $dotenv = Dotenv\Dotenv::createImmutable("C:\\xampp\htdocs\\");
-        $dotenv->load();
-
-        $this->databaseHost = $_ENV['DB_SERVER_NAME'];
-        $this->databaseName = $_ENV['DB_NAME'];
-
+    }
+    static function  getInsatace(): SingletonConnection{
+        if(self::$instace == null){
+            self::$instace = new MySqlPDOConnection();
+        }
+        return self::$instace;
+    } 
+    function createDatabaseConnection(){
         try {
+            $dotenv = Dotenv\Dotenv::createImmutable("C:\\xampp\htdocs\\");
+            $dotenv->load();
+
+            $this->databaseHost = $_ENV['DB_SERVER_NAME'];
+            $this->databaseName = $_ENV['DB_NAME'];
 
             $this->pdo = new PDO("mysql:host=$this->databaseHost;dbname=$this->databaseName",'root','');
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
