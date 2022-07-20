688d56ec893d4daae4ecd3c110f30dd7cf5650a5
Can <yamahasirossi@gmail.com>
2022-07-20 21:52:06 +0300

    replaced to singleton instance

--- src/data_acces_layer/DatabaseConnections/SqliteInMemoryConnection.php
+++ src/data_acces_layer/DatabaseConnections/SqliteInMemoryConnection.php
@@ -1,18 +1,32 @@
 <?php
 require '/xampp/htdocs/vendor/autoload.php';
-class SqliteInMemoryConnection implements DatabaseConnection {
+class SqliteInMemoryConnection implements SingletonConnection {
+    private static $instance;
     private PDO $pdo;
-    function __construct()
-    {
-        try {
-            $this->pdo = new PDO('sqlite::memory:');
-        } catch (\Exception $th) {
-            echo $th->getMessage();
+    private function __construct(){}
+    static function getInstance():SingletonConnection{
+        if(self::$instance == null){
+            self::$instance = new SqliteInMemoryConnection();
         }
-
+        return self::$instance;
     }
     function getConnection():PDO
     {
         return $this->pdo;
     }
+	/**
+	 *
+	 * @return mixed
+	 */
+	function createDatabaseConnection() {
+        {
+            try {
+                $this->pdo = new PDO('sqlite::memory:');
+            } catch (\Exception $th) {
+                echo $th->getMessage();
+            }
+    
+        }
+    
+	}
 }
