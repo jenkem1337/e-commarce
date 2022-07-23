27435c246433cfd8943af86969b9c9a22022d3e1
Can <yamahasirossi@gmail.com>
2022-07-23 14:27:59 +0300

    idk how to explain this commit

--- src/controller/AuthController.php
+++ src/controller/AuthController.php
@@ -21,30 +21,39 @@ class AuthController {
 
         $jsonBody = json_decode(file_get_contents('php://input'));
         $userLoginDto = new UserLoginDto($jsonBody->email, $jsonBody->password); 
-        $response = $this->authService->login($userLoginDto);
-        if($response->isSuccess()){
-           $refreshTokenModel = $response->getRefreshToken();
-           $issuedAt = time();
-           $expireTime = $issuedAt + (60 * 60 * 24);
-            $payload = [
-                "exp"=> $expireTime ,
-                "iat"=>$issuedAt,
-                "user_uuid"=>$response->getUuid(),
-                "full_name"=>$response->getFullName(),
-                "email"=>$response->getEmail(),
-                "user_role"=>$response->getUserRole()
-            ];
+        $res = $this->authService->login($userLoginDto);
+        $res->onSucsess(function($response){
             
-            $token = JWT::encode($payload,  $_ENV["SECRET_KEY"], "HS256");
-            http_response_code(200);
+                $refreshTokenModel = $response->getRefreshToken();
+                $issuedAt = time();
+                $expireTime = $issuedAt + (60 * 60 * 24);
+                 $payload = [
+                     "exp"=> $expireTime ,
+                     "iat"=>$issuedAt,
+                     "user_uuid"=>$response->getUuid(),
+                     "full_name"=>$response->getFullName(),
+                     "email"=>$response->getEmail(),
+                     "user_role"=>$response->getUserRole()
+                 ];
+                 
+                 $token = JWT::encode($payload,  $_ENV["SECRET_KEY"], "HS256");
+                 http_response_code(200);
+                 echo json_encode([
+                     "message"=>"Login Process Succesful",
+                     "access_token"=> $token,
+                     "refresh_token"=>$refreshTokenModel->getRefreshToken()
+                 ]);
+     
+        })->onError(function($err){
             echo json_encode([
-                "message"=>"Login Process Succesful",
-                "access_token"=> $token,
-                "refresh_token"=>$refreshTokenModel->getRefreshToken()
+                'error_message' => $err->getErrorMessage(),
+                'status_code' => $err->getErrorCode()
             ]);
-            die();
-        }
+            http_response_code($err->getErrorCode());
+        });
+        
     }
+    
 
     function register(){
         $jsonBody = json_decode(file_get_contents('php://input'));
@@ -59,20 +68,29 @@ class AuthController {
             date ('Y-m-d H:i:s')
         );
 
-        $response = $this->authService->register($userCreationalDto);
-        if($response->isSuccess()){
-            http_response_code(201);
+        $res = $this->authService->register($userCreationalDto);
+        $res->onSucsess(function($response){
+            
+                http_response_code(201);
+                echo json_encode([
+                    'uuid'=>$response->getUuid(),
+                    'full_name'=>$response->getFullname(),
+                    'email'=>$response->getEmail(),
+                    'is_user_activated'=>$response->getIsUserActivaed(),
+                    'activation_message'=> "Verification mail has been sended",
+                    'created_at'=>$response->getCreated_at(),
+                    'updated_at'=>$response->getUpdated_at()
+                ]);
+            
+    
+        })->onError(function($err){
             echo json_encode([
-                'uuid'=>$response->getUuid(),
-                'full_name'=>$response->getFullname(),
-                'email'=>$response->getEmail(),
-                'is_user_activated'=>$response->getIsUserActivaed(),
-                'activation_message'=> "Verification mail has been sended",
-                'created_at'=>$response->getCreated_at(),
-                'updated_at'=>$response->getUpdated_at()
+                'error_message' => $err->getErrorMessage(),
+                'status_code' => $err->getErrorCode()
             ]);
-            die();
-        }
+            http_response_code($err->getErrorCode());
+
+        });
 
     }
 
@@ -80,47 +98,60 @@ class AuthController {
             
             $jsonBody = json_decode(file_get_contents('php://input'));
         
-            $response = $this->authService->refreshToken(new RefreshTokenDto($jsonBody->refresh_token));
-            if($response->isSuccess()){
-                $refreshTokenModel = $response->getRefreshToken();
-                $issuedAt = time();
-                $expireTime = $issuedAt + (60 * 60 * 24);
-                 $payload = [
-                     "exp"=> $expireTime ,
-                     "iat"=>$issuedAt,
-                     "user_uuid"=>$response->getUuid(),
-                     "full_name"=>$response->getFullName(),
-                     "email"=>$response->getEmail(),
-                     "user_role"=>$response->getUserRole()
-                 ];
-                 
-                 $token = JWT::encode($payload,  $_ENV["SECRET_KEY"], "HS256");
-                 http_response_code(201);
-                 echo json_encode([
-                     "message"=>"Token Refreshed",
-                     "access_token"=> $token,
-                     "refresh_token"=>$refreshTokenModel->getRefreshToken()
-                 ]);
-                 die();
-             }
+            $res = $this->authService->refreshToken(new RefreshTokenDto($jsonBody->refresh_token));
+            $res->onSucsess(function($response){
+                
+                    $refreshTokenModel = $response->getRefreshToken();
+                    $issuedAt = time();
+                    $expireTime = $issuedAt + (60 * 60 * 24);
+                     $payload = [
+                         "exp"=> $expireTime ,
+                         "iat"=>$issuedAt,
+                         "user_uuid"=>$response->getUuid(),
+                         "full_name"=>$response->getFullName(),
+                         "email"=>$response->getEmail(),
+                         "user_role"=>$response->getUserRole()
+                     ];
+                     
+                     $token = JWT::encode($payload,  $_ENV["SECRET_KEY"], "HS256");
+                     http_response_code(201);
+                     echo json_encode([
+                         "message"=>"Token Refreshed",
+                         "access_token"=> $token,
+                         "refresh_token"=>$refreshTokenModel->getRefreshToken()
+                     ]);
+                
     
- 
+            })->onError(function($err){
+                echo json_encode([
+                    'error_message' => $err->getErrorMessage(),
+                    'status_code' => $err->getErrorCode()
+                ]);    
+                http_response_code($err->getErrorCode());
+
+            });
     }
     function verifyUserAccount(){
         $code = isset($_GET['code']) ? $_GET['code'] :"";
-        $response = $this->authService->verifyUserAccount(new EmailVerificationDto($code));
-        if($response->isSuccess()){
-            http_response_code(200);
+        $res = $this->authService->verifyUserAccount(new EmailVerificationDto($code));
+        
+        $res->onSucsess(function($response){
+                http_response_code(200);
+                echo json_encode([
+                    'uuid'=>$response->getUuid(),
+                    'full_name'=>$response->getFullname(),
+                    'email'=>$response->getEmail(),
+                    'is_user_activated'=>$response->getIsUserActivaed(),
+                    'created_at'=>$response->getCreated_at(),
+                    'updated_at'=>$response->getUpdated_at()
+            ]);    
+        })->onError(function($err){
             echo json_encode([
-                'uuid'=>$response->getUuid(),
-                'full_name'=>$response->getFullname(),
-                'email'=>$response->getEmail(),
-                'is_user_activated'=>$response->getIsUserActivaed(),
-                'created_at'=>$response->getCreated_at(),
-                'updated_at'=>$response->getUpdated_at()
-            ]);
-            die();
+                'error_message' => $err->getErrorMessage(),
+                'status_code' => $err->getErrorCode()
+            ]);    
+            http_response_code($err->getErrorCode());
 
-        }
+        });
     }
 }
