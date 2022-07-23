cf85a256db532f23566e7a8397419bf2760d64df
Can <yamahasirossi@gmail.com>
2022-07-23 14:25:54 +0300

    injected TransactionalDecorator

--- src/controller/factory/AuthControllerFactory.php
+++ src/controller/factory/AuthControllerFactory.php
@@ -4,19 +4,22 @@ use PHPMailer\PHPMailer\PHPMailer;
 class AuthControllerFactory implements Factory{
     function createInstance($isMustBeConcreteObject = false, ...$params):AuthController {
         return new AuthController(
-            new AuthServiceImpl(
-                new UserRepositoryAggregateRootDecorator(
-                    new UserRepositoryImpl(
-                        new UserDaoImpl(MySqlPDOConnection::getInsatace()), 
-                        new RefreshTokenDaoImpl(new RedisConnection()),
-                        new ConcreteUserFactory(),
-                        new ConcreteRefreshTokenFactory()
-                    )
-                ),
-                new EmailServiceImpl(new PHPMailer(true)),
-                new ConcreteUserFactory(),
-                new ConcreteRefreshTokenFactory()
-
-            ));
+            new TransactionalAuthServiceDecorator(
+                new AuthServiceImpl(
+                    new UserRepositoryAggregateRootDecorator(
+                        new UserRepositoryImpl(
+                            new UserDaoImpl(MySqlPDOConnection::getInsatace()), 
+                            new RefreshTokenDaoImpl(new RedisConnection()),
+                            new ConcreteUserFactory(),
+                            new ConcreteRefreshTokenFactory()
+                        )
+                    ),
+                    new EmailServiceImpl(new PHPMailer(true)),
+                    new ConcreteUserFactory(),
+                    new ConcreteRefreshTokenFactory()
+    
+                )
+            )
+        );
     }
 }
