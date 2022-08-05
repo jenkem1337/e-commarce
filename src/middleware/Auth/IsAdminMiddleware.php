d766dbcc981bbb9ef4b0abca2910aedbc0e617f8
Can <yamahasirossi@gmail.com>
2022-08-05 17:46:01 +0300

    try catch added

--- src/middleware/Auth/IsAdminMiddleware.php
+++ src/middleware/Auth/IsAdminMiddleware.php
@@ -4,11 +4,22 @@ require "./vendor/autoload.php";
 class IsAdminMiddleware extends Middleware {
     function check(): bool
     {
-        $jwtPayloadDto = JwtPayloadDto::getInstance();
-        $payload = $jwtPayloadDto->getPayload();
-        if($payload->user_role !== "ADMIN"){
-            throw new UserRoleException('admin');
+        try {
+            $jwtPayloadDto = JwtPayloadDto::getInstance();
+            $payload = $jwtPayloadDto->getPayload();
+            if($payload->user_role !== "ADMIN"){
+                throw new UserRoleException('admin');
+            }
+            return parent::check();
+    
+        } catch (Exception $e) {
+            return (new ErrorResponseDto($e->getMessage(), $e->getCode()))
+                                        ->onError(function(ErrorResponseDto $err){
+                                            json_encode([
+                                                'error-message' => $err->getErrorMessage(),
+                                                'status-code' => $err->getErrorCode()
+                                            ]);
+                                        });
         }
-        return parent::check();
     }
 }
--- src/middleware/Auth/JwtAuthMiddleware.php
+++ src/middleware/Auth/JwtAuthMiddleware.php
@@ -9,19 +9,31 @@ $dotenv->load();
 
 class JwtAuthMiddleware extends Middleware {
     function check():bool{
+        try {
+            $headers = apache_request_headers();
+        
+            if(!$headers['Authorization']) throw new NullException("authorization header");
+            
+            $authorization = explode(' ', $headers['Authorization']);
+            $jwt = $authorization[1];
+            
+            if(!$jwt) throw new DoesNotExistException('jwt token');
+            
+            $data = JWT::decode($jwt, new Key($_ENV['SECRET_KEY'], "HS256"));
+            $jwtPayloadDto = JwtPayloadDto::getInstance();
+            $jwtPayloadDto->setPayload($data);
+            return parent::check();
+    
+        } catch (Exception $e) {
+            return (new ErrorResponseDto($e->getMessage(), $e->getCode()))
+                                        ->onError(function(ErrorResponseDto $err){
+                                            json_encode([
+                                                'error-message' => $err->getErrorMessage(),
+                                                'status-code' => $err->getErrorCode()
+                                            ]);
+                                        });
 
-        $headers = apache_request_headers();
-        
-        if(!$headers['Authorization']) throw new NullException("authorization header", 401);
-        
-        $authorization = explode(' ', $headers['Authorization']);
-        $jwt = $authorization[1];
-        
-        if(!$jwt) throw new DoesNotExistException('jwt token', 401);
-        
-        $data = JWT::decode($jwt, new Key($_ENV['SECRET_KEY'], "HS256"));
-        $jwtPayloadDto = JwtPayloadDto::getInstance();
-        $jwtPayloadDto->setPayload($data);
-        return parent::check();
+
+        }
     }
 }
