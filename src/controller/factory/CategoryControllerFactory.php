ce3c2b4f4ff8d2e75e0358bab63fb2f2ff8a3f79
Can <yamahasirossi@gmail.com>
2022-08-05 18:55:09 +0300

    added new depenedencies

--- src/controller/factory/CategoryControllerFactory.php
+++ src/controller/factory/CategoryControllerFactory.php
@@ -4,14 +4,21 @@ require './vendor/autoload.php';
 class CategoryControllerFactory implements Factory {
     function createInstance($isMustBeConcreteObjcet = false, ...$params): CategoryController
     {
-        $productRepositoryImpl =new ProductRepositoryImpl(new ProductFactoryContext([
-            ProductFactory::class => new ConcreteProductFactory(),
-            ProductCategoryCreationalModelFactory::class => new ConcreteProductCategoryCreationalModelFactory()
-        ]));
+        $productRepositoryImpl = new ProductRepositoryImpl(
+            new ProductFactoryContext([
+                ProductFactory::class => new ConcreteProductFactory(),
+                ProductCategoryCreationalModelFactory::class => new ConcreteProductCategoryCreationalModelFactory()
+            ]),
+            new ProductDaoImpl(MySqlPDOConnection::getInsatace())
+        );
+        
+        
         $categoryRepositoryImpl = new CategoryRepositoryImpl(
             new CategoryDaoImpl(MySqlPDOConnection::getInsatace()),
             new  ConcreteCategoryFactory()
         );
+        
+        
         $categoryRepositoryImpl->setProductMediator($productRepositoryImpl);
         return new CategoryController(
                 new TransactionalProductServiceDecorator(
