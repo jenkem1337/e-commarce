fab253793c8ba2bcb3f1e79fc16f284c88ab2d35
Can <yamahasirossi@gmail.com>
2022-08-05 18:57:32 +0300

    added new repositories

--- src/repository/repository_mediator/AbstractProductRepositoryMediatorComponent.php
+++ src/repository/repository_mediator/AbstractProductRepositoryMediatorComponent.php
@@ -1,8 +1,16 @@
 <?php
 abstract class AbstractProductRepositoryMediatorComponent {
     protected CategoryRepository $categoryRepository;
+    protected ImageRepository $imageRepository;
+    protected CommentRepository $commentRepository;
 
     function setCategoryRepository(CategoryRepository $categoryRepository){
         $this->categoryRepository = $categoryRepository;
+    }
+    function setImageRepository(ImageRepository $imgRepo){
+        $this->imageRepository = $imgRepo;
+    }
+    function setCommentRepository(CommentRepository $commentRepository){
+        $this->categoryRepository = $commentRepository;
     }
 } 
