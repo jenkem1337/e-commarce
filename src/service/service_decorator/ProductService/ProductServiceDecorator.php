b3cbcdf9aea7f189983d8204d5f99effd6c2b69c
Can <yamahasirossi@gmail.com>
2022-08-09 21:39:18 +0300

    deleted unnecessary methods

--- src/service/service_decorator/ProductService/ProductServiceDecorator.php
+++ src/service/service_decorator/ProductService/ProductServiceDecorator.php
@@ -11,57 +11,11 @@ abstract class ProductServiceDecorator implements ProductService {
 	{
 		return $this->productService->craeteNewProduct($dto);
 	}
-	function uploadImageForProduct(ImageCreationalDto $dto): ResponseViewModel
+	function updateProductBrandName(ChangeProductBrandNameDto $dto): ResponseViewModel{
+		return $this->productService->updateProductBrandName($dto);
+	}
+	function findOneProductByUuid(FindOneProductByUuidDto $dto): ResponseViewModel
 	{
-		return $this->productService->uploadImageForProduct($dto);
-	}
-	/**
-	 *
-	 * @param CategoryCreationalDto $dto
-	 *
-	 * @return ResponseViewModel
-	 */
-	function createNewCategory(CategoryCreationalDto $dto): ResponseViewModel {
-        return $this->productService->createNewCategory($dto);
-	}
-	
-	/**
-	 *
-	 * @param FindCategoryByUuidDto $dto
-	 *
-	 * @return ResponseViewModel
-	 */
-	function findOneCategoryByUuid(FindCategoryByUuidDto $dto): ResponseViewModel {
-        return $this->productService->findOneCategoryByUuid($dto);
-	}
-	
-	/**
-	 *
-	 * @param UpdateCategoryNameByUuidDto $dto
-	 *
-	 * @return ResponseViewModel
-	 */
-	function updateCategoryNameByUuid(UpdateCategoryNameByUuidDto $dto): ResponseViewModel {
-        return $this->productService->updateCategoryNameByUuid($dto);
-	}
-	
-	/**
-	 *
-	 * @param FindAllCategoryDto $dto
-	 *
-	 * @return ResponseViewModel
-	 */
-	function findAllCategory(FindAllCategoryDto $dto): ResponseViewModel {
-        return $this->productService->findAllCategory($dto);
-	}
-	
-	/**
-	 *
-	 * @param DeleteCategoryDto $dto
-	 *
-	 * @return ResponseViewModel
-	 */
-	function deleteCategoryByUuid(DeleteCategoryDto $dto): ResponseViewModel {
-        return $this->productService->deleteCategoryByUuid($dto);
+		return $this->productService->findOneProductByUuid($dto);
 	}
 }
