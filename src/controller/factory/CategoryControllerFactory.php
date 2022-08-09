4a8cfc0dede506ef2cf3aa902c341900e1058378
Can <yamahasirossi@gmail.com>
2022-08-09 21:36:47 +0300

    added new repository implementations and deleted TransactionalProductService instead of TransactionalCategoryService

--- src/controller/factory/CategoryControllerFactory.php
+++ src/controller/factory/CategoryControllerFactory.php
@@ -9,6 +9,7 @@ class CategoryControllerFactory implements Factory {
                 ProductFactory::class => new ConcreteProductFactory(),
                 ProductCategoryCreationalModelFactory::class => new ConcreteProductCategoryCreationalModelFactory()
             ]),
+            new ConcreteProductSubscriberFactory,
             new ProductDaoImpl(MySqlPDOConnection::getInsatace())
         );
         
@@ -17,22 +18,44 @@ class CategoryControllerFactory implements Factory {
             new CategoryDaoImpl(MySqlPDOConnection::getInsatace()),
             new  ConcreteCategoryFactory()
         );
+        $rateRepositoryImpl = new RateRepositoryImpl(
+            new RateDaoImpl(MySqlPDOConnection::getInsatace()),
+            new ConcreteRateFactory()
+        );
+        $commentRepositoryImpl = new CommentRepositoryImpl(
+            new CommentDaoImpl(MySqlPDOConnection::getInsatace()),
+            new ConcreteCommentFactory()
+        );
+        $imageRepositoryImpl = new ImageRepositoryImpl(
+            new ImageDaoImpl(MySqlPDOConnection::getInsatace()),
+            new ConcreteImageFactory()
+        );
+        $userRepositoryImpl = new UserRepositoryImpl(
+            new UserDaoImpl(MySqlPDOConnection::getInsatace()),
+            new RefreshTokenDaoImpl(new RedisConnection),
+            new ConcreteUserFactory,
+            new ConcreteRefreshTokenFactory
+        );
+        $userRepositoryImpl->setProductMediator($productRepositoryImpl);
+        $commentRepositoryImpl->setProductMediator($productRepositoryImpl);
+        $rateRepositoryImpl->setProductMediator($productRepositoryImpl);
+        $imageRepositoryImpl->setProductMediator($productRepositoryImpl);
+        $categoryRepositoryImpl->setProductMediator($productRepositoryImpl);
         
         
         $categoryRepositoryImpl->setProductMediator($productRepositoryImpl);
         return new CategoryController(
-                new TransactionalProductServiceDecorator(
-                    new ProductServiceImpl(
-                        $productRepositoryImpl,
-                    new ProductFactoryContext([
-                        ProductFactory::class => new ConcreteProductFactory(),
-                        ProductCategoryCreationalModelFactory::class => new ConcreteProductCategoryCreationalModelFactory()
-                    ]),
-                    new  ConcreteCategoryFactory(),
-                    new  ConcreteImageFactory(),
-                    new UploadServiceImpl()
-                    ), MySqlPDOConnection::getInsatace()
-                )
-            );
+                    new TransactionalCategoryServiceDecorator(
+                        new CategoryServiceImpl(
+                            $productRepositoryImpl,
+                            new ConcreteCategoryFactory,
+                            new ProductFactoryContext([
+                                ProductFactory::class => new ConcreteProductFactory(),
+                                ProductCategoryCreationalModelFactory::class => new ConcreteProductCategoryCreationalModelFactory()
+                            ])
+                        ),MySqlPDOConnection::getInsatace()
+                    )
+                );
+        
     }
 }
