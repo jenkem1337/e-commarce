fccce603399202222b9b6c956125c47b2b0fe079
Can <yamahasirossi@gmail.com>
2022-08-09 21:39:27 +0300

    deleted unnecessary methods

--- src/service/ProductService.php
+++ src/service/ProductService.php
@@ -1,11 +1,9 @@
 <?php
 
 interface ProductService {
+    //product
     function craeteNewProduct(ProductCreationalDto $dto):ResponseViewModel;
-    function uploadImageForProduct(ImageCreationalDto $dto): ResponseViewModel;
-    function createNewCategory(CategoryCreationalDto $dto):ResponseViewModel;
-    function findOneCategoryByUuid(FindCategoryByUuidDto $dto):ResponseViewModel;
-    function updateCategoryNameByUuid(UpdateCategoryNameByUuidDto $dto):ResponseViewModel;
-    function findAllCategory(FindAllCategoryDto $dto):ResponseViewModel;
-    function deleteCategoryByUuid(DeleteCategoryDto $dto): ResponseViewModel;
+    function findOneProductByUuid(FindOneProductByUuidDto $dto): ResponseViewModel;
+    function updateProductBrandName(ChangeProductBrandNameDto $dto): ResponseViewModel;
+
 }
