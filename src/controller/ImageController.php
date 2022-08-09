a86a1d4226cc05663119b695537341c090315d49
Can <yamahasirossi@gmail.com>
2022-08-09 23:23:21 +0300

    commit

--- src/controller/ImageController.php
+++ src/controller/ImageController.php
@@ -1,21 +1,53 @@
 <?php
+
+use Ramsey\Uuid\Uuid;
+
 require './vendor/autoload.php';
 class ImageController {
-    private ProductService $productService;
-    function __construct(ProductService $pService)
+
+    private ImageService $imageService;
+    function __construct(ImageService $ImageService)
     {
-        $this->productService = $pService;
+        $this->imageService = $ImageService;
     }
     function uploadImageForProduct($productUuid){
-        $imageArray = array(
-            'name' => $_FILES['images']['name'],
-            'tmp_name'=> $_FILES['images']['tmp_name']
-        );
-        $response = $this->productService->uploadImageForProduct(new ImageCreationalDto($imageArray, $productUuid));
-        $response->onSucsess(function (ImageCreatedResponseDto $responseDto){
+        $imageArrayItarator = new ArrayIterator([]);
+        $imageArr = [];
+        foreach((array) $_FILES['images']['tmp_name'] as $k => $v){
+            $imageArr['uuid']= Uuid::uuid4();
+            $imageArr['productUuid'] = $productUuid;
+            $imageArr['imageName'] = $_FILES['images']['name'][$k];
+            $imageArr['imageTmpName'] = $_FILES['images']['tmp_name'][$k];
+            $imageArr['createdAt'] =date ('Y-m-d H:i:s');
+            $imageArr['updatedAt'] = date ('Y-m-d H:i:s');
+            $imageObject = json_decode(json_encode($imageArr),false);
+            $imageArrayItarator->append($imageObject);
+        }
+        $this->imageService->uploadImageForProduct(
+            new ImageCreationalDto(
+                $imageArrayItarator,
+                $productUuid
+            )
+            
+            )->onSucsess(function (ImageCreatedResponseDto $responseDto){
             echo json_encode([
                 'images' => $responseDto->getImages()
-          ]);
+                ]);
+            
+            })->onError(function (ErrorResponseDto $err){
+                echo json_encode([
+                    'error_message' => $err->getErrorMessage(),
+                    'status_code'=> $err->getErrorCode()
+                ]);
+            });
+        
+    }
+    function deleteImageByUuid($imageUuid, $productUuid){
+        $this->imageService->deleteImageByUuid(new DeleteImageByUuidDto(
+            $productUuid,
+            $imageUuid
+        ))->onSucsess(function (ImageDeletedResponseDto $response){
+            echo json_encode(['success_message'  => $response->getSuccessMessage()]);
         })->onError(function (ErrorResponseDto $err){
             echo json_encode([
                 'error_message' => $err->getErrorMessage(),
