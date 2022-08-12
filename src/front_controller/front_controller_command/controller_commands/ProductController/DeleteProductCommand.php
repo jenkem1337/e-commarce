93528b0f8e4c09d16afe02b74932498d45f97321
Can <yamahasirossi@gmail.com>
2022-08-13 01:47:08 +0300

    initial commit

---
+++ src/front_controller/front_controller_command/controller_commands/ProductController/DeleteProductCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class DeleteProductCommand implements Command {
+    private ProductController $controller;
+    private Middleware $jwtMiddleware;
+    function __construct(ProductController $cont, Middleware $middleware)
+    {
+        $this->controller = $cont;
+        $this->jwtMiddleware = $middleware;
+    }
+
+    function process($params = [])
+    {
+            $this->jwtMiddleware->linkWith(new IsAdminMiddleware());
+            $this->jwtMiddleware->check();
+            return $this->controller->deleteProduct(...$params);
+    }
+}
