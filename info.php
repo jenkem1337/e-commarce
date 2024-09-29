770c309b825a4877334da322e9f6c10db48295ff
Can <yamahasirossi@gmail.com>
2024-09-29 21:16:00 +0300

    commit

--- info.php
+++ info.php
@@ -1,18 +1,2 @@
 <?php
-$conf = new RdKafka\Conf();
-$conf->set('metadata.broker.list', 'broker:9092');
-
-$producer = new RdKafka\Producer($conf);
-$topic = $producer->newTopic('test_topic');
-
-for ($i = 0; $i < 10; $i++) {
-    $topic->produce(RD_KAFKA_PARTITION_UA, 0, "Message $i");
-    $producer->poll(0);
-}
-
-while ($producer->getOutQLen() > 0) {
-    $producer->poll(50);
-}
-
-echo "Messages produced successfully!";
-
+phpinfo();
