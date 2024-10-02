f135cd61bc3919afbfb476f8f8da524a9a1d5d5f
Can <yamahasirossi@gmail.com>
2024-10-02 19:06:25 +0300

    location property and static factories added

--- src/Product/Core/Domain/Image.php
+++ src/Product/Core/Domain/Image.php
@@ -3,23 +3,26 @@
 class Image extends BaseEntity implements ImageInterface{
     private $productUuid;
     private $imageName;
+    private $location;
 
-    function __construct($uuid, $productUuid, $imageName, $createdAt, $updatedAt){
+    function __construct($uuid, $productUuid, $imageName, $location, $createdAt, $updatedAt){
         parent::__construct($uuid, $createdAt, $updatedAt);
         if(!$productUuid) throw new NullException('product uuid');
         if(!$imageName) throw new NullException('image name');
+        if(!$location) throw new NullException('image name');
         $this->productUuid = $productUuid;
         $this->imageName = $imageName;
+        $this->location = $location;
     }
-    public static function newInstance($uuid, $productUuid, $imageName, $createdAt, $updatedAt):ImageInterface {
+    public static function newInstance($uuid, $productUuid, $imageName, $location, $createdAt, $updatedAt):ImageInterface {
         try {
-            return new Image($uuid, $productUuid, $imageName, $createdAt, $updatedAt);
+            return new Image($uuid, $productUuid, $imageName, $location, $createdAt, $updatedAt);
         } catch (\Throwable $th) {
             return new NullImage();
         }
     }
-    public static function newStrictInstance($uuid, $productUuid, $imageName, $createdAt, $updatedAt):ImageInterface {
-        return new Image($uuid, $productUuid, $imageName, $createdAt, $updatedAt);
+    public static function newStrictInstance($uuid, $productUuid, $imageName, $location,$createdAt, $updatedAt):ImageInterface {
+        return new Image($uuid, $productUuid, $imageName,$location, $createdAt, $updatedAt);
     }
     /**
      * Get the value of productUuid
@@ -35,5 +38,9 @@ class Image extends BaseEntity implements ImageInterface{
     public function getImageName()
     {
         return $this->imageName;
+    }
+
+    public function getLocation(): mixed {
+        return $this->location;
     }
 }
