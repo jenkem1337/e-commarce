231c115feb7dcadc72f59a1455a09901136ab67e
Can <yamahasirossi@gmail.com>
2024-11-21 23:02:40 +0300

    implemented some methods PaymentServiceImpl

--- src/Payment/Application/Service/PaymentServiceImpl.php
+++ src/Payment/Application/Service/PaymentServiceImpl.php
@@ -3,9 +3,35 @@
 class PaymentServiceImpl implements PaymentService {
     function __construct(
         private PaymentRepository $paymentRepository,
-        private PaymentGateway $paymentGateway
+        private PaymentGateway $paymentGateway,
     ){}
-    function payWithCreditCart(PayWithCreditCartDto $dto){
-        $payment = Payment::payWithCreditCart()
+    private function payWithCreditCart(PayWithCreditCartDto $dto):ResponseViewModel{
+        $payment = Payment::payWithCreditCart(
+            $dto->getUserUuid(),
+            $dto->getAmount(),
+            $dto->getCartNumber(),
+            $dto->getCartExpirationDate(),
+            $dto->getCartCVC(),
+            $dto->getCartOwner() 
+        );
+        
+        $this->paymentGateway->payWithCreditCart($payment);
+        $this->paymentRepository->saveChanges($payment);
+        return new SuccessResponse([
+            "data" => [
+                "uuid" => $payment->getUuid()
+            ]
+        ]);
+    }
+
+    function pay(PayOrderDto $dto): ResponseViewModel {
+        $response = null;
+        switch($dto->getPaymentMethod()){
+            case "CreditCart":
+                $response = $this->payWithCreditCart(new PayWithCreditCartDto($dto->getUserUuid(), $dto->getPaymentMethod(), $dto->getOrderAmount(), $dto->getPaymentDetail()->number, $dto->getPaymentDetail()->expiration_date, $dto->getPaymentDetail()->cvc, $dto->getPaymentDetail()->owner));
+                break;
+            default: throw new Exception("Unknown payment command");
+        }
+        return $response;
     }
 }
