5314e9e07b4febe4907b7c5141d2762259cf4508
Can <yamahasirossi@gmail.com>
2024-11-21 23:12:46 +0300

    added createShippingForOrderCreation

--- src/Shipping/Core/Interface/ShippingService.php
+++ src/Shipping/Core/Interface/ShippingService.php
@@ -3,4 +3,5 @@
 interface ShippingService {
     function findAll(FindAllShippingMethodDto $dto):ResponseViewModel;
     function findOneByUuid(FindOneShippingMethodDto $dto): ResponseViewModel;
+    function createShippingForOrderCreation(CreationalShippingDto $dto): ResponseViewModel;
 }
