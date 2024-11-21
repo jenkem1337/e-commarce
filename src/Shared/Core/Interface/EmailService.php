1a1e361cd56e1df2eb89a0e2ef2d1d6be4e0cdbb
Can <yamahasirossi@gmail.com>
2024-11-21 23:09:37 +0300

    added notifyUserForOrderCreated method

--- src/Shared/Core/Interface/EmailService.php
+++ src/Shared/Core/Interface/EmailService.php
@@ -4,4 +4,5 @@ interface EmailService {
     function sendVerificationCode(User $user);
     function sendChangeForgettenPasswordEmail(User $user);
     function notifyProductSubscribersForPriceChanged(SendPriceReducedEmailDto $priceReducedEmailDto);
+    function notifyUserForOrderCreated(PlaceOrderDto $placeOrderDto);
 }
