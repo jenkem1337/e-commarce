0dbc8509c2b182d962410376d0d37ed150fe76df
Can <yamahasirossi@gmail.com>
2024-11-21 23:08:44 +0300

    deleted unnecessary methods and findManyAggregateByUuids method implemented

--- src/Product/Infrastructure/Repository/Decorator/ProductRepositoryDecorator.php
+++ src/Product/Infrastructure/Repository/Decorator/ProductRepositoryDecorator.php
@@ -9,10 +9,6 @@ abstract class ProductRepositoryDecorator implements ProductRepository {
     function saveChanges($e){
         $this->productRepository->saveChanges($e);
     }
-    function createProduct(Product $p)
-    {
-        $this->productRepository->createProduct($p);
-    }
     function findProductsByCriteria($filter)
     {
         return $this->productRepository->findProductsByCriteria($filter);
@@ -33,16 +29,12 @@ abstract class ProductRepositoryDecorator implements ProductRepository {
         return $this->productRepository->findOneProductByUuid($uuid, $filter);
 
     }
-    function persistImage(Product $p)
-    {
-        $this->productRepository->persistImage($p);
-    }
-    function deleteImageByUuid($uuid)
-    {
-        $this->productRepository->deleteImageByUuid($uuid);
-    }
     function findOneProductWithOnlySubscriberByUuid($uuid, $userUuid): ProductInterface{
         return $this->productRepository->findOneProductWithOnlySubscriberByUuid($uuid, $userUuid);
+    }
+
+    function findManyAggregateByUuids($uuids): ProductCollection {
+        return $this->productRepository->findManyAggregateByUuids($uuids);
     }
  
 }
