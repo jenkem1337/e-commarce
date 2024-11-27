60cb6f0a629bbdcdc5d0eda0eebaf2cec30821fd
Can <yamahasirossi@gmail.com>
2024-11-28 00:07:14 +0300

    implemented completeCheckout($checkoutUuid) method

--- src/Orders/Application/Service/RemoteCheckoutServiceImpl.php
+++ src/Orders/Application/Service/RemoteCheckoutServiceImpl.php
@@ -6,8 +6,15 @@ class RemoteCheckoutServiceImpl implements RemoteCheckoutService {
     ){}
     function findAmoundAndItemsByUuid($checkoutUuid, $jwtToken):ResponseViewModel {
         $checkout = $this->remoteCheckoutDataSource->findAmoundAndItemsByUuid($checkoutUuid, $jwtToken);
+        if($checkout->checkoutState !== "CHECKOUT_CREATED") {
+            throw new CheckoutStateNotSuitableException();
+        }
         return new SuccessResponse([
             "data" => $checkout
         ]);
+    }
+
+    function completeCheckout($checkoutUuid) {
+        $this->remoteCheckoutDataSource->completeCheckout($checkoutUuid);
     }
 }
--- src/Orders/Infrastructure/RemoteDataSource/HttpCheckoutServiceDataSource.php
+++ src/Orders/Infrastructure/RemoteDataSource/HttpCheckoutServiceDataSource.php
@@ -1,18 +1,31 @@
 <?php
 use GuzzleHttp\Client;
 class HttpCheckoutServiceDataSource implements RemoteCheckoutDataSource {
-    private Client $client;
+    private Client $checkoutReadClient;
+    private Client $checkoutWriteClient;
     function __construct(){
-        $this->client = new Client([
+        $this->checkoutReadClient = new Client([
             "base_uri" => $_ENV["CHECKOUT_READ_SERVICE_URI"]
+        ]);
+        $this->checkoutWriteClient = new Client([
+            "base_uri" => $_ENV["CHECKOUT_WRITE_SERVICE_URI"]
         ]);
     }
     function findAmoundAndItemsByUuid($checkoutUuid, $jwtToken){
-        $response = $this->client->request("GET", "/api/v1/checkout/".$checkoutUuid, [
+        $response = $this->checkoutReadClient->request("GET", "/api/v1/checkout/".$checkoutUuid, [
                         'headers' => [
                             'Authorization' => 'Bearer ' . $jwtToken,
                         ]   
                     ]);
         return json_decode($response->getBody()->getContents());
+    }
+
+    function completeCheckout($checkoutUuid){
+        $jwtToken = explode(' ', $_SERVER['HTTP_AUTHORIZATION'])[1];
+            $this->checkoutWriteClient->request("POST", "/api/v1/checkout/complete/" . $checkoutUuid, [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $jwtToken,
+                ],
+            ]);
     }
 }
