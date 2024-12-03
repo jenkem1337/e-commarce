063d616917fe29d61e1a9650f0461f2c070f5df4
Can <yamahasirossi@gmail.com>
2024-12-03 16:26:32 +0300

    methods implemented

--- src/Orders/Application/Service/OrderServiceImpl.php
+++ src/Orders/Application/Service/OrderServiceImpl.php
@@ -9,13 +9,16 @@ class OrderServiceImpl implements OrderService {
         private EmailService $emailService
     ){}
 
-    function placeOrder(PlaceOrderDto $placeOrderDto){
+    function placeOrder(PlaceOrderDto $placeOrderDto): ResponseViewModel{
         
         $order = Order::placeOrder(
             $placeOrderDto->getUserUuid(),
             $placeOrderDto->getAmount(),
             $placeOrderDto->getOrderItems()
         );
+
+        $this->productService->checkQuantityAndDecrease(new CheckAndDecreaseProductsDto($order->getItems()));
+
         $shipmentResponse = $this->shippingService->createShippingForOrderCreation(new CreationalShippingDto($order->getAmount(), $placeOrderDto->getAddressTitle(),$placeOrderDto->getAddressOwnerName(),$placeOrderDto->getAddressOwnerSurname(),$placeOrderDto->getFullAddress(),$placeOrderDto->getAddressCountry(),$placeOrderDto->getAddressProvince(),$placeOrderDto->getAddressDistrict(),$placeOrderDto->getAddressZipCode()))
                                             ->getData();
         $order->determineLatestAmount($shipmentResponse["price"]);
@@ -26,18 +29,70 @@ class OrderServiceImpl implements OrderService {
         
         $order->setStateToProcessingAndAssignPaymentAndShippingUuid($paymentResponse["uuid"], $shipmentResponse["uuid"]);
         
-        $this->productService->checkQuantityAndDecrease(new CheckAndDecreaseProductsDto($order->getItems()));
-
         $this->orderRepository->saveChanges($order);
         
         //$this->emailService->notifyUserForOrderCreated($placeOrderDto);
         return new SuccessResponse([
             "message" => "Order created successfully !!",
             "data" => [
-                "uuid" => $order->getUuid(),
-                "amount" => $order->getAmount(),
-                "order_items" => $order->getItems()
+                "order_uuid" => $order->getUuid(),
             ]
         ]);
     }
+
+    function findByCriteria() {}
+    
+    //TODO This method is in progress.
+    function cancelOrder(OrderStatusDto $dto):ResponseViewModel{
+        $orderAggregate = $this->orderRepository->findOneAggregateByUuid($dto->getUuid());
+
+        if($orderAggregate->isNull()) throw new NotFoundException("order");
+
+        $this->paymentService->refund(new RefundPaymentDto($orderAggregate->getPaymentUuid()));
+
+        $this->shippingService->setStateToCanceled(new ShippingStatusDto($orderAggregate->getShippingUuid()));
+
+
+        return new SuccessResponse([]);
+    }
+    
+    function orderDelivered(OrderStatusDto $dto): ResponseViewModel{
+        $orderAggregate = $this->orderRepository->findOneAggregateByUuid($dto->getUuid());
+        
+        if($orderAggregate->isNull()) throw new NotFoundException("order");
+        
+        $this->shippingService->isShipmentDelivered(new IsShippingDeliveredDto($orderAggregate->getShippingUuid()));
+
+        $orderAggregate->setStatusToDelivered();
+        
+        $this->orderRepository->saveChanges($orderAggregate);
+        
+        return new SuccessResponse([
+            "message" => "Order successfully delivered.",
+            "data" => [
+                "uuid" => $orderAggregate->getUuid()
+            ]
+        ]);
+
+    }
+    function shipOrder(OrderStatusDto $dto):ResponseViewModel{
+        $orderAggregate = $this->orderRepository->findOneAggregateByUuid($dto->getUuid());
+        
+        if($orderAggregate->isNull()) throw new NotFoundException("order");
+        
+        $this->shippingService->setStateToDispatched(new ShippingStatusDto($orderAggregate->getShippingUuid()));
+        
+        $orderAggregate->setStatusToDispatched();
+        
+        $this->orderRepository->saveChanges($orderAggregate);
+        
+        return new SuccessResponse([
+            "message" => "Order successfully dispatched.",
+            "data" => [
+                "uuid" => $orderAggregate->getUuid()
+            ]
+        ]);
+    }
+    function refundOrder(OrderStatusDto $dto){}
+    
 }
