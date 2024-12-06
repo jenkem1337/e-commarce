08a3381b207c71f3b1534a463766a24a45d76b7d
Can <yamahasirossi@gmail.com>
2024-12-06 14:08:06 +0300

    findOneAggregateWithItemsByUuid implemented

--- src/Orders/Infrastructure/Repository/OrderRepositoryImpl.php
+++ src/Orders/Infrastructure/Repository/OrderRepositoryImpl.php
@@ -12,6 +12,35 @@ class OrderRepositoryImpl extends TransactionalRepository implements OrderReposi
         $this->orderDao->saveChanges($order);
     }
 
+    function findOneAggregateWithItemsByUuid($uuid):OrderInterface {
+        $orderObj = $this->orderDao->findOneByUuid($uuid);
+        $orderItemsObj = $this->orderDao->findAllItemsByOrderUuid($uuid);
+        $itemCollection = new OrderItemCollection();
+        foreach($orderItemsObj as $item) {
+            $itemCollection->add(
+                OrderItem::newInstance(
+                    $item->uuid,
+                    $item->order_uuid,
+                    $item->product_uuid,
+                    $item->quantity,
+                    $item->created_at,
+                    $item->updated_at
+                )
+            );
+        }
+
+        return Order::newInstanceWithItems(
+            $orderObj->uuid,
+            $orderObj->user_uuid,
+            $orderObj->payment_uuid,
+            $orderObj->shipping_uuid,
+            $orderObj->amount,
+            $orderObj->status,
+            $itemCollection,
+            $orderObj->created_at,
+            $orderObj->updated_at
+        );
+    }
     function findOneAggregateByUuid($uuid):OrderInterface{
         $orderObject = $this->orderDao->findOneByUuid($uuid); 
         return Order::newInstance(
