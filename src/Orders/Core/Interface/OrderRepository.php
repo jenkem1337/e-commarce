d69edb4b029ab4088a8946655a213a3df7f66e3c
Can <yamahasirossi@gmail.com>
2024-12-06 14:07:44 +0300

    findOneAggregateWithItemsByUuid metod added

--- src/Orders/Core/Interface/OrderRepository.php
+++ src/Orders/Core/Interface/OrderRepository.php
@@ -3,4 +3,5 @@
 interface OrderRepository {
     function saveChanges(Order $order);
     function findOneAggregateByUuid($uuid):OrderInterface;
+    function findOneAggregateWithItemsByUuid($uuid): OrderInterface;
 }
